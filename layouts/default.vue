e340b3b37f91272e5ae359e3c6a382336d2ea5d0
farnazii <ffarzipour92@gmail.com>
2021-09-25 16:48:01 +0330

    add skils

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,38 @@
 <template>
 	<div class="fz__parent">
-		<div class="fz__box">
-			<div class="fz__info-box">
-				<div class="fz__image"></div>
-				<div class="fz__name">
-					<h2>Farnaz Farzipour</h2>
+		<div class="container">
+			<div class="fz_sub_container">
+				<div class="fz__box">
+					<div class="fz__info-box">
+						<div class="fz__container">
+							<div class="fz__image"></div>
+							<div class="fz__name">
+								<h2>Farnaz Farzipour</h2>
+							</div>
+							<div class="fz__title">
+								<h6>Front-End Developer</h6>
+							</div>
+							<div class="fz__social">
+								<img src="~/assets/icons/instagram.svg" alt="" />
+								<img src="~/assets/icons/telegram.png" alt="" />
+								<img src="~/assets/icons/telegram.png" alt="" />
+								<img src="~/assets/icons/telegram.png" alt="" />
+							</div>
+							<div class="fz__button">
+								<button class="fz__cv_button">
+									<a href="">Download CV</a>
+								</button>
+							</div>
+						</div>
+						<div class="fz__copyright">© 2020 All rights reserved.</div>
+					</div>
+					<div class="fz__contect-box_container">
+						<Nuxt class="fz__contect-box" />
+					</div>
 				</div>
-				<div class="fz__title">
-					<h6>Front-End Developer</h6>
-				</div>
-				<div class="fz__social">
-					<img src="~/assets/icons/instagram.svg" alt="" />
-				</div>
-				<div class="fz__button"></div>
-			</div>
-			<div class="fz__contect-box_container">
-				<Nuxt class="fz__contect-box" />
+				<div class="fz__navigator"></div>
 			</div>
 		</div>
-		<div class="fz__navigator"></div>
 	</div>
 </template>
 <script>
@@ -33,7 +47,14 @@ export default {
 	height: 100vh;
 	background-color: #f7e9ff;
 	padding: 10vh 120px;
-	display: flex;
+
+	.container {
+		height: 100%;
+	}
+	.fz_sub_container {
+		display: flex;
+		height: 100%;
+	}
 
 	.fz__navigator {
 		height: 300px;
@@ -59,36 +80,75 @@ export default {
 			height: 100%;
 			display: flex;
 			flex-direction: column;
-			align-items: center;
-			justify-content: center;
-			padding: 10px;
-			.fz__image {
-				position: relative;
-				width: 250px;
-				height: 250px;
-				background-color: #fff;
-				border-radius: 50%;
-				border: 3px solid #fff;
-			}
-			.fz__name {
-				margin-top: 20px;
-				h2 {
-					color: #fff;
-					font-weight: bold;
+			justify-content: space-between;
+			padding: 20px 10px;
+
+			.fz__container {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				justify-content: center;
+				.fz__image {
+					position: relative;
+					width: 200px;
+					height: 200px;
+					background-color: #fff;
+					border-radius: 50%;
+					border: 3px solid #fff;
+					margin-top: 50px;
+				}
+				.fz__name {
+					margin-top: 20px;
+					h2 {
+						color: #fff;
+						font-weight: bold;
+					}
+				}
+				.fz__title {
+					h6 {
+						color: #fff;
+						margin-top: 10px;
+					}
+				}
+				.fz__social {
+					margin-top: 30px;
+					width: 100%;
+					display: flex;
+					justify-content: center;
+
+					img {
+						width: 20px;
+						height: 20px;
+						object-fit: contain;
+						margin-right: 10px;
+					}
+				}
+				.fz__cv_button {
+					width: 200px;
+					height: 50px;
+					border-radius: 30px;
+					border: 2px solid #fff;
+					background-color: transparent;
+					margin-top: 50px;
+					box-shadow: 0 0 30px -5px rgb(0 0 0 / 15%);
+					a {
+						width: 100%;
+						height: 100%;
+						text-decoration: none;
+						color: #fff !important;
+						&:hover {
+							text-decoration: none;
+							color: #fff;
+						}
+					}
 				}
 			}
-			.fz__title {
-				h6 {
-					color: #fff;
-					margin-top: 10px;
-				}
-			}
-			.fz__social {
-				img {
-					width: 30px;
-					height: 30px;
-					object-fit: contain;
-				}
+
+			.fz__copyright {
+				color: #fff;
+				width: 100%;
+				font-size: 14px;
+				text-align: center;
 			}
 		}
 
